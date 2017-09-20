9cbac40cf1a58272d4ac6a475a86784f0b6108fe
John Michael U. Santos <jsantos@jsantos.local>
2017-09-20 15:27:59 +0800

    wip

--- client/app/components/simon/simon.controller.js
+++ client/app/components/simon/simon.controller.js
@@ -2,6 +2,22 @@ class SimonController {
   constructor($state, $stateParams) {
     "ngInject";
     this.name = 'simon';
+    this.buttons = [];
+    this.initializeButtons();
+  }
+
+  initializeButtons = () => {
+    for (let i = 1; i <= 4; i++) {
+      this.buttons.push({
+        id: i,
+        class: `key-button-${i}`,
+        backslash: (i == 2 || i == 3)
+      });
+    }
+  };
+
+  test = (i) => {
+    alert(i);
   }
 }
 
