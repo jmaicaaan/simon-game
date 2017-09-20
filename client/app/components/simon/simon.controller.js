5e487c0a1e042331b2bb5cfbd7b76081c28dc3f4
jmaicaaan <jmsantosss29@yahoo.com>
2017-09-20 22:46:57 +0800

    Resolved #3

--- client/app/components/simon/simon.controller.js
+++ client/app/components/simon/simon.controller.js
@@ -3,6 +3,8 @@ class SimonController {
     "ngInject";
     this.name = 'simon';
     this.buttons = [];
+    this.demoKeys = [];
+    this.userKeys = [];
     this.initializeButtons();
   }
 
@@ -16,9 +18,34 @@ class SimonController {
     }
   };
 
-  test = (i) => {
-    alert(i);
-  }
+  start = (clearDemoKeys) => {
+    if (clearDemoKeys) this.demoKeys = [];
+  };
+
+  addDemoKey = () => {
+    this.demoKeys.push(this.getRandomNumber());
+  };
+
+  getRandomNumber = () => {
+    let randomNumber = Math.floor(Math.random(1 * 10) * 4) + 1;
+    return randomNumber;
+  };
+  // todo: pre-cache the mp3 resource
+  playSound = (keyId) => {
+    if (!keyId) return;
+    let links = {
+      1: 'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3', 
+      2: 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
+      3: 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', 
+      4: 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'
+    };
+    return new Audio(links[keyId])
+      .play();
+  };
+
+  onKeyPress = (buttonKey) => {
+    this.playSound(buttonKey.id);
+  };
 }
 
 export default SimonController;
